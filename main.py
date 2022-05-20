c7377957185f4fd48c9ca422c258efb5a224f67a
STUDENT17 <ahmad.hafiiz@fiwa.sch.id>
2022-05-20 03:23:54 -0700

    second commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+
+
+value=input(f'nama kamu siapa\n')
+print (f'halo {value}')
+year=(int(input(f'halo {value},umurmu berapa\n')))
+work=input(f'kerja lu apa anjing\n')
+print(f'nama:{value}\n umur:{year}\n kerja:{work}')
